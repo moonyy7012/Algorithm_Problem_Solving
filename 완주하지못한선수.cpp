5415ead2f92a13c9ba147daefcd7a4c81eb39daa
moonyy7012 <moonyy7012@naver.com>
2021-05-12 18:26:57 +0900

    Add files via upload

--- 완주하지못한선수.cpp
+++ 완주하지못한선수.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
@@ -8,11 +9,12 @@ string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
     sort(participant.begin(), participant.end());
     sort(completion.begin(), completion.end());
-
-    for(int i=0; i<completion.size(); i++){
-        if(participant[i]!=completion[i]) return participant[i];
-
+    for(int i=0, len=participant.size(); i<len; i++){
+        if(participant[i]!=completion[i]) {  //c++에서 문자열 비교 ==, != 로 가능
+            answer += participant[i];
+            break;
+        }
     }
 
-    return participant[participant.size()-1];
+    return answer;
 }
