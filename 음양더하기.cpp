14375176c708e9e2c3262b31cf347f5b08bee5da
moonyy7012 <moonyy7012@naver.com>
2021-05-20 17:07:23 +0900

    Add files via upload

---
+++ 음양더하기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    for(int i=0, len=absolutes.size(); i<len; i++)
+        answer+=(absolutes[i]*(signs[i]?1:(-1)));
+    
+    return answer;
+}
