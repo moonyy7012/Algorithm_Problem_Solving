233bbcc24200ca67d580d9b2206c43dcb1cf482c
moonyy7012 <moonyy7012@naver.com>
2021-11-17 22:42:42 +0900

    Add files via upload

--- 프로그래머스/소수찾기.cpp
+++ 프로그래머스/소수찾기.cpp
@@ -1,17 +1,44 @@
 #include <string>
 #include <vector>
-
+#include <iostream>
+#include <set>
+#include <cmath>
 using namespace std;
+set<int> johap;
+int answer = 0;
 
-int solution(int n) {
-    int answer = 0;
-    vector<bool> v(1000001, false);
-    for(int i=2; i<=n/2; i++){
-        for(int j=2; i*j<=n; j++)
-            v[i*j]=true;
-    }
-    for(int i=2; i<=n; i++)
-        if(!(v[i]))answer++;
+void makeJohap(string now, string numbers, vector<bool> visited) {  //모든 부분집합 찾기
+	for (int i = 0; i < numbers.length(); i++) {
+		if (visited[i]) continue;
+		string next = now + numbers[i];
+		visited[i] = true;
+		johap.insert(stoi(next)); //중복 없는 set에 집어넣기
+		makeJohap(next, numbers, visited);
+		visited[i] = false;
+	}
 
-    return answer;
+
+}
+
+//소수 찾는 가지치기 - 2는 소수/1이하 제외/일의자리 0인 수 제외/2외의짝수제외/3부터 제곱근까지의 홀수를 나누는 수로 하여 나누어떨어지는 것이 있는지 확인
+void isPrime(int num) { //해당 숫자가 소수인지 판별
+	if (num == 2) { answer++; return; }
+	if (num <= 1 || !(num & 1) || !(num % 2)) return;
+	for (int i = 3, last = sqrt(num); i <= last; i += 2) {
+		if (!(num%i)) return;
+	}
+	answer++;
+}
+
+int solution(string numbers) {
+	vector<bool> visited(numbers.length());
+	makeJohap("", numbers, visited);
+
+	for (auto i = johap.begin(); i != johap.end(); i++) { //구한 부분집합들이 각각 소수인지 판별
+		cout << *i << " ";
+		isPrime(*i);
+	}
+
+
+	return answer;
 }
